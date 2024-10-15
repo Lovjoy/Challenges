2c68e8739db872244beb4764dd0e9a9e52a18d6d
Ray Hobbs <hobbsmath@gmail.com>
2024-10-15 11:04:45 -0400

    Created binary code that prints out binary numbare 1-100 and takes an input of a binary and converts it into base 10

--- BaseConverter/src/Main.java
+++ BaseConverter/src/Main.java
@@ -1,22 +1,42 @@
+import java.util.Scanner;
+// Ray Joy
+// 10/14/2024
+// Prints all binary numbers 0-100
+// Convert a binary number into a base ten decimal
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder newNumbers = new StringBuilder();
+        int oldNumber;
+        int newDigit;
+        String binaryNumber;
+        // Prints all the Binary numbers 0-100
+        System.out.println("0 = 0");
+        for (int i = 1; i <= 100; i++) {
+            oldNumber = i;
+            while (oldNumber != 0) {
+                newDigit = oldNumber % 2;
+                newNumbers.insert(0, newDigit);
+                oldNumber = oldNumber / 2;
+            }
+            System.out.println(i + " = " + newNumbers);
+            newNumbers = new StringBuilder();
+        }
+        // Converts a Binary number into base 10 decimal
+        char binaryDigit;
+        double newBaseTen;
+        double oldBaseTen = 0;
+        System.out.println("Enter a Binary Number: ");
+        binaryNumber = scanner.nextLine();
 
-        String charters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String newNumbers = "";
-        char newDigit = '0';
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 2; j++) {
-                if ((i % 2) < j) {
-                    newDigit = charters.charAt(j);
-                    newNumbers += newDigit;
-                } else {
-                    newDigit = charters.charAt(j);
-                    newNumbers = newNumbers.substring(0, (newNumbers.length()-1));
-                    newNumbers += newDigit;
-                }
+        for (int i = 1; i <= binaryNumber.length(); i++) {
+            binaryDigit = binaryNumber.charAt(binaryNumber.length() - i);
+            if (binaryDigit == '1') {
+                newBaseTen =  Math.pow(2, i-1);
+                oldBaseTen += newBaseTen;
 
-                System.out.println(newNumbers);
             }
         }
-        }
+        System.out.printf("%s = %.0f", binaryNumber, oldBaseTen);
     }
+}
