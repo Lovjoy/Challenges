c1fd5dffb9db51280dec498a9430451f96497eb0
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2024-10-27 09:11:16 -0400

    Made Stats dynamic with selecting array size max and min, also corrected some math for q1 and q3

--- Stats/src/Main.java
+++ Stats/src/Main.java
@@ -4,25 +4,43 @@
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
 
-        int n = 30;
-        int[] numbers = new int[n];
+        int n;              // Total number of random elements
+
         int temp;
-        String modes = "";
-        int[] mode = new int[n];
+        String modes = "";  // Used to print list of modes
+
         int modeCounter;
-        double sum = 0;
+        double sum = 0;     // used for mean
+        int max;            // largest value element can be
+        int min;            // smallest value element can be
 
+        System.out.print("Enter total number of elements: ");
+        n = sc.nextInt();
+
+        int[] numbers = new int[n];
+        int[] mode = new int[n];
+
+        System.out.print("Enter highest possible value: ");
+        max = sc.nextInt();
+
+        System.out.print("Enter lowest possible value: ");
+        min = sc.nextInt();
+
+        // Creates random array with size of input n and possible range of min - max
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (rand.nextInt(201) - 100);
+            numbers[i] = (rand.nextInt(max - min) + min);
         }
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers));   // Unsorted numbers
 
+        // Sorts numbers from least to greatest
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i; j < numbers.length - 1; j++) {
                 if (numbers[j + 1] < numbers[i]) {
@@ -32,9 +50,11 @@ public class Main {
                 }
             }
         }
+        // Used for mean
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
+        // Creates new array that counts is used to count how many of that kind of element in the array
         for (int i = 0; i < numbers.length - 2; i++) {
             for (int j = i; j < numbers.length; j++) {
                 if (numbers[i] == numbers[j]) {
@@ -42,24 +62,36 @@ public class Main {
                 }
             }
         }
+        // Finds the largest number in mode list
         modeCounter = 1;
         for (int i = 0; i < mode.length - 1; i++) {
             if (mode[i] > modeCounter) {
                 modeCounter = mode[i];
             }
         }
+        // Adds all modes with that amount of modeCounter to modes list
         for (int i = 0; i < mode.length - 1; i++) {
             if (mode[i] == modeCounter) {
                 modes += (numbers[i] + ", ");
             }
         }
+        // Prints sorted numbers
         System.out.println(Arrays.toString(numbers));
+
         System.out.println("Min: " + numbers[0]);
-        System.out.println("Q1: " + numbers[n / 4]);
-        System.out.println("Median: " + numbers[n / 2]);
-        System.out.println("Q3: " + numbers[(3 * n) / 4]);
+
+        if (n % 2 == 0) {
+            System.out.println("Q1: " + numbers[n / 4]);
+            System.out.println("Median: " + (double)((numbers[(n - 1) / 2] + (numbers[((n - 1)/ 2) + 1])) / 2));
+            System.out.println("Q3: " + numbers[(3 * n) / 4]);
+        } else {
+            System.out.println("Q1: " + (double)((numbers[(n / 4) - 1]) + numbers[n / 4]) / 2);
+            System.out.println("Median: " + ((numbers[n / 2] )));
+            System.out.println("Q3: " + (double)((numbers[(3 * n) / 4 ]) + numbers[(3 * n) / 4 + 1]) / 2);
+        }
         System.out.println("Max: " + numbers[n - 1]);
         System.out.printf("Mean: %.2f\n", (sum / n));
+
         if (modeCounter == 1) {
             System.out.println("No Mode.");
         } else {
